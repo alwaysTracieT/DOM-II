0bd3df3f0ea33381076ed3e7e71d111c45bb59d5
Tracie T <traciecodes@gmail.com>
2019-06-18 16:04:34 -0500

    first commit. added three events.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+//LOGO SIZE CHANGE
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function(event){
+    event.target.style.transform = "scale(1.2)";
+});  
+
+//WELCOME POP UP
+const openPop = document.querySelector('.open-popup');
+const closePop = document.querySelector('.close-popup');
+const popup = document.querySelector('.popup');
+const hidePopupListener = {
+    handleEvent: function(){
+        popup.setAttribute('hidden', '');
+    }
+};
+openPop.addEventListener('click', function(){
+    popup.removeAttribute('hidden');
+});
+closePop.addEventListener('click', hidePopupListener);
+
+//FONT CHANGE ON NAV OPTIONS
+const navOptions = document.querySelector('.nav-link');
+navOptions.addEventListener('mouseover', function(event){
+    event.target.style.fontWeight = "bold";
+});
+
